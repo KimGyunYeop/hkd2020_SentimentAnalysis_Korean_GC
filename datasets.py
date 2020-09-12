2321c5712eb549dda5278d6151294fa6c3544d97
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-12 19:08:00 +0900

    Delete Others

--- datasets.py
+++ datasets.py
@@ -14,9 +14,9 @@ class BaseDataset(Dataset):
         self.tokenizer = tokenizer
         self.maxlen = args.max_seq_len
         if "train" in mode:
-            data_path = os.path.join(args.data_dir, args.data_dir, args.train_file)
+            data_path = os.path.join(args.data_dir, args.data_dir, args.data_dir, args.train_file)
         elif "dev" in mode:
-            data_path = os.path.join(args.data_dir, args.data_dir, args.dev_file)
+            data_path = os.path.join(args.data_dir, args.data_dir,args.data_dir,  args.dev_file)
         elif "test" in mode:
             data_path = os.path.join(args.data_dir, args.task, args.test_file)
         self.dataset = pd.read_csv(data_path, encoding="utf8", sep="\t")
@@ -41,9 +41,9 @@ class KOSACDataset(Dataset):
         self.tokenizer = tokenizer
         self.maxlen = args.max_seq_len
         if "train" in mode:
-            data_path = os.path.join(args.data_dir, args.data_dir, args.train_file)
+            data_path = os.path.join(args.data_dir,args.data_dir, args.data_dir, args.train_file)
         elif "dev" in mode:
-            data_path = os.path.join(args.data_dir, args.data_dir, args.dev_file)
+            data_path = os.path.join(args.data_dir,args.data_dir, args.task, args.dev_file)
         elif "test" in mode:
             data_path = os.path.join(args.data_dir, args.task, args.test_file)
 
