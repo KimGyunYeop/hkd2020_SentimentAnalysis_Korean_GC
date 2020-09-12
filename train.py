19515de85347e015cbede157b2b51039f930c59a
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-12 19:00:25 +0900

    Delete Others

--- train.py
+++ train.py
@@ -265,11 +265,8 @@ def main(cli_args):
     args.output_dir = os.path.join(args.ckpt_dir, cli_args.result_dir)
     args.model_mode = cli_args.model_mode
 
-    if cli_args.small == True:
-        args.ckpt_dir = args.ckpt_dir + "_small"
-    else:
-        if os.path.exists(args.output_dir):
-            raise ValueError("result path is already exist(path = %s)" % args.output_dir)
+    if os.path.exists(args.output_dir):
+        raise ValueError("result path is already exist(path = %s)" % args.output_dir)
 
     init_logger()
     set_seed(args)
