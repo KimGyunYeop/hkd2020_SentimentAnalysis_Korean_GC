122373d145b8ad5e1a60773af1f472fbc392cc99
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-14 13:09:08 +0900

    Add Kosac and VoSenti_for_Word

--- model.py
+++ model.py
@@ -126,6 +126,133 @@ class StarV_ANN(nn.Module):
 
         return result
 
+class KOSAC_LSTM_ATT(nn.Module):
+    def __init__(self, model_type, model_name_or_path, config):
+        super(KOSAC_LSTM_ATT, self).__init__()
+        self.emb = MODEL_ORIGINER[model_type].from_pretrained(
+            model_name_or_path,
+            config=config)
+
+        # Embedding
+        self.input_embedding = self.emb.embeddings.word_embeddings
+        self.polarity_embedding = nn.Embedding(5, 768)
+        self.intensity_embedding = nn.Embedding(5, 768)
+
+        self.lstm = nn.LSTM(768, 768, batch_first=True, bidirectional=False)
+        self.lstm_dropout = nn.Dropout(0.2)
+        self.dense = nn.Linear(768, 768)
+        self.dropout = nn.Dropout(0.2)
+        self.out_proj = nn.Linear(768, 2)
+        self.gelu = nn.GELU()
+
+        self.att_w = nn.Parameter(torch.randn(1, 768, 1))
+
+    def re_attention(self, lstm_output, final_h, input):
+        batch_size, seq_len = input.shape
+
+        final_h = final_h.squeeze()
+
+        # final_h.size() = (batch_size, hidden_size)
+        # output.size() = (batch_size, num_seq, hidden_size)
+        # lstm_output(batch_size, seq_len, lstm_dir_dim)
+        att = torch.bmm(torch.tanh(lstm_output),
+                        self.att_w.repeat(batch_size, 1, 1))
+        att = F.softmax(att, dim=1)  # att(batch_size, seq_len, 1)
+        att = torch.bmm(lstm_output.transpose(1, 2), att).squeeze(2)
+        attn_output = torch.tanh(att)  # attn_output(batch_size, lstm_dir_dim)
+        return attn_output
+
+    def forward(self, input_ids, attention_mask, labels, token_type_ids, polarity_ids, intensity_ids):
+        # embedding
+        input_emb_result = self.input_embedding(input_ids)
+        polarity_emb_result = self.polarity_embedding(polarity_ids)
+        intensity_emb_result = self.intensity_embedding(intensity_ids)
+
+        embedding_result = input_emb_result + polarity_emb_result / 100 + intensity_emb_result / 100
+
+        outputs = self.emb(input_ids=None, attention_mask=attention_mask, token_type_ids=token_type_ids,
+                           inputs_embeds=embedding_result)
+        outputs, (h, c) = self.lstm(outputs[0])
+
+        attn_output = self.re_attention(outputs, h, input_ids)
+
+        outputs = self.dense(attn_output)
+        outputs = self.gelu(outputs)
+        outputs = self.dropout(outputs)
+        outputs = self.out_proj(outputs)
+
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(outputs.view(-1, 2), labels.view(-1))
+
+        result = (loss, outputs)
+        return result
+
+class VoSenti_for_Word(nn.Module):
+    def __init__(self, model_type, model_name_or_path, config):
+        super(VoSenti_for_Word, self).__init__()
+        self.emb = MODEL_ORIGINER[model_type].from_pretrained(
+            model_name_or_path,
+            config=config)
+        self.config = config
+        self.lstm = nn.LSTM(768, 768, batch_first=True, bidirectional=False)
+
+        #sentiment module
+        self.word_dense = nn.Linear(768, 2)
+        self.sentiment_embedding = nn.Embedding(2, 768)
+        self.softmax = nn.Softmax(dim=-1)
+        # attention module
+        self.att_w = nn.Parameter(torch.randn(1, 768, 1))
+
+        self.dense = nn.Linear(768, 768)
+        self.dropout = nn.Dropout(0.2)
+        self.out_proj = nn.Linear(768, 2)
+        self.gelu = nn.GELU()
+
+    def attention_net(self, lstm_output, input):
+        batch_size, seq_len = input.shape
+
+        att = torch.bmm(torch.tanh(lstm_output),
+                        self.att_w.repeat(batch_size, 1, 1))
+        att = F.softmax(att, dim=1)  # att(batch_size, seq_len, 1)
+        att = torch.bmm(lstm_output.transpose(1, 2), att).squeeze(2)
+        attn_output = torch.tanh(att)  # attn_output(batch_size, lstm_dir_dim)
+        return attn_output
+
+    def sentiment_net(self, lstm_outputs):
+        result = self.word_dense(lstm_outputs)
+        sig_output = self.softmax(result)
+        #for i, output in enumerate(sig_output.tolist()):
+        #    print("vector:",i,output)
+        batch_size, max_len, _=sig_output.shape
+        zeros = torch.zeros(batch_size, max_len, dtype=torch.long).to(self.config.device)
+        ones = torch.ones(batch_size, max_len, dtype=torch.long).to(self.config.device)
+        emb_result = self.sentiment_embedding(zeros) * sig_output[:,:,0].unsqueeze(-1).repeat(1,1,768) + self.sentiment_embedding(ones) * sig_output[:,:,1].unsqueeze(-1).repeat(1,1,768)
+        senti_output = self.gelu(lstm_outputs + emb_result)
+        return senti_output
+
+    def forward(self, input_ids, attention_mask, labels, token_type_ids):
+        # embedding
+        emb_output = self.emb(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+
+        sentiment_outputs = self.sentiment_net(emb_output[0])
+
+        outputs, (h,_) = self.lstm(sentiment_outputs)
+
+        # attention
+        attention_outputs = self.attention_net(outputs,input_ids)
+
+        outputs = self.dense(attention_outputs)
+        outputs = self.gelu(outputs)
+        outputs = self.dropout(outputs)
+        outputs = self.out_proj(outputs)
+
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(outputs.view(-1, 2), labels.view(-1))
+
+        result = (loss, outputs)
+
+        return result
+
 
 class ENSEMBLE_MODEL(nn.Module):
     def __init__(self, model_type, model_name_or_path, config):
@@ -225,5 +352,7 @@ class ENSEMBLE_MODEL(nn.Module):
 MODEL_LIST = {
     "StarV_ANN": StarV_ANN,
     "StarV_AM" : StarV_AM,
+    "KOSAC_LSTM_ATT": KOSAC_LSTM_ATT,
+    "VoSenti_for_Word": VoSenti_for_Word,
     "ENSEMBLE_MODEL":ENSEMBLE_MODEL
 }
