7beb23690c62d7a4f3a3608a7b10b9daeaf894e7
KimGyunYeop <gyop0817@naver.com>
2020-09-28 18:21:57 +0900

    docker

--- datasets.py
+++ datasets.py
@@ -101,5 +101,6 @@ DATASET_LIST = {
     "KOSAC_LSTM_ATT": BaseDataset,
     "VoSenti_for_Word": BaseDataset,
     "FINAL_MODEL" : BaseDataset,
+    "FINAL_MODEL_NN":BaseDataset,
     "Star_Label_AM_att": BaseDataset
 }
